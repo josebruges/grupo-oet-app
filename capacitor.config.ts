85f38f9ac06469893b2ad92222c7f96e41cded92
Jose Brugés Cervantes <josebruges@MacBook-Pro-de-Jose.local>
2024-02-15 19:20:01 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'grupo-oet-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
